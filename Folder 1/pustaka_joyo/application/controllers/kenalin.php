6b7cf08809b5f60724f08cadd75bf2628a2d8e57
joyouripe1 <joyouripe@gmail.com>
2020-10-28 16:45:32 +0700

    bebas

---
+++ Folder 1/pustaka_joyo/application/controllers/kenalin.php
@@ -0,0 +1,10 @@
+<?php
+class kenalin extends CI_Controller{
+    public function index() {
+        echo "<h1> perkenalan</h1>";
+        echo "Tri Joyo P<br>
+                saya tinggal di Trayeman<br>
+                saya suka olahraga futsal dan lari";
+    }
+
+}
---
+++ Folder 1/pustaka_joyo/application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Tri</li>
+        <li>Nama Belakang : joyo</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Trayeman rt 02/01</li> </ul>
+    
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+    <li>Tegal</li></ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Berenang</li>
+        <li>jogging</li>
+    </ul>
+</section>
